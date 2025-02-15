0042c1138bb3c28b5d0c28745998dbc293a83eca
Terezinha Freire C Sousa <terezinhafcs@id.uff.br>
2025-02-15 19:53:25 -0300

    Update app.js - retirei comentários

--- app.js
+++ app.js
@@ -1,48 +1,48 @@
-//Array para guardar os nomes dos amigos
+
 let listaDeAmigos = [];
 
 // Função para adicionar um amigo à lista
  
 function adicionarAmigo() {
-    // Obtém o valor digitado no input
+    
     let inputAmigo = document.getElementById('amigo');
-    let amigo = inputAmigo.value.trim(); // Remove espaços extras antes e depois do nome
+    let amigo = inputAmigo.value.trim(); 
 
-    // Verifica se o campo está vazio
+    
     if (amigo === "") {
         alert("Digite um nome válido!");
         return; // Sai da função se o nome for inválido
-    }else if (listaDeAmigos.includes(amigo)) //depois o segundo caso de invalido
+    }else if (listaDeAmigos.includes(amigo)) 
     {
         alert("Nome informado já está na sua lista");
         limparCampo();
         return;
     }
 
-    // Adiciona o nome à lista de amigos
+    
     listaDeAmigos.push(amigo);
     
-    // Limpa o campo de entrada após adicionar o nome
+   
     inputAmigo.value = '';
 
-  // Atualiza a exibição da lista de amigos na tela
+ 
     atualizarLista();   
 }
 
- // Função para atualizar a lista de amigos na interface do usuário
+ 
  
 function atualizarLista() {
-    // Obtém o elemento da lista no HTML
+   
     let listaElemento = document.getElementById('listaAmigos');
     
-    // Limpa a lista antes de recriá-la para evitar duplicação
+    
     listaElemento.innerHTML = '';
 
-    // Percorre a lista de amigos e adiciona cada um como um item <li>
+    
     for (let i = 0; i < listaDeAmigos.length; i++) {
-        let li = document.createElement('li'); // Cria um elemento de lista <li>
-        li.textContent = listaDeAmigos[i]; // Define o texto do <li> com o nome do amigo
-        listaElemento.appendChild(li); // Adiciona o <li> à lista no HTML
+        let li = document.createElement('li'); 
+        li.textContent = listaDeAmigos[i];
+        listaElemento.appendChild(li); 
     }
 }
 function limparCampo(){
@@ -51,13 +51,13 @@ function limparCampo(){
 }
 
 function sortearAmigo() {
-    //Se alista de amigos não tiver pelo menos dois amigos...mensagem de erro
+   
     if (listaDeAmigos.length < 2) {
         alert("Adicione pelo menos dois amigos para o sorteio!"); 
         return;
     }
 
-    // Usando for para percorrer e escolher um nome aleatório
+   
     let indiceSorteado = Math.floor(Math.random() * listaDeAmigos.length);
     let sorteado;
 
